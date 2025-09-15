1dac38035d1b0d8a65791c253359785dd0236cba
Zachary Reece <zachdreece@gmail.com>
2025-09-15 03:06:14 -0600

    added analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import theme from './styles/theme.ts'
-import { ThemeProvider } from '@mui/material/styles'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import theme from "./styles/theme.ts";
+import { ThemeProvider } from "@mui/material/styles";
+import { Analytics } from "@vercel/analytics/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <Analytics />
       <App />
     </ThemeProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
